f16ea78a77fb16fbb4a93b293731321257673477
VladD <dmitriev.vs.751@mail.ru>
2020-11-07 17:38:49 +0300

    Struct (authoring), adding speed value

---
+++ Assets/RW/Scripts/ECS/ComponentData/MoveForwardComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+// interface for implementing general-purpose components
+// any implementation must be a struct
+[GenerateAuthoringComponent]
+public struct MoveForward : IComponentData
+{
+    public float speed;
+}
